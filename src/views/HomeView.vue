a7b0af547c0efe65596a45140e1fae5d9d59f2c9
Roduan Kareem Aldeen <roduan98@gmail.com>
2022-04-22 05:39:35 +0300

    finish home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,50 @@
 <template>
   <div class="home">
-    <!--  -->
+    <section class="hero is-fullheight">
+      <div class="hero-body">
+        <div class="container">
+          <div class="columns">
+            <div class="column is-6 is-md-6">
+              <animated-letters class="mb-4" />
+              <h1 class="title is-3">
+                المتعة والمرح في تعلم العربية
+              </h1>
+              <div class="columns">
+                <div class="column is-4">
+                  <router-link
+                    class="button is-primary is-block is-rounded"
+                    to="#"
+                  >
+                    هَيَّا نَتَعَلَّم
+                  </router-link>
+                </div><div class="column is-4">
+                  <router-link
+                    class="button is-primary is-block is-rounded is-outlined"
+                    to="#"
+                  >
+                    تسجيل الدخول
+                  </router-link>
+                </div>
+              </div>
+            </div>
+            <div class="column is-md-6">
+              <img
+                src="@/assets/penguin.svg"
+                alt=""
+                class="image mx-auto"
+              >
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
+import AnimatedLetters from '@/components/home/AnimatedLetters'
 export default {
   name: 'HomeView',
-  components: {},
+  components: { AnimatedLetters },
 }
 </script>
