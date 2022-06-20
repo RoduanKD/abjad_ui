8460899fe08793229237e03f7464e0b61375b692
Roduan Kareem Aldeen <roduan98@gmail.com>
2022-06-20 19:37:12 +0300

    add youtube support to video tutorial exercise

--- src/components/letters/exercises/VideoTutorial.vue
+++ src/components/letters/exercises/VideoTutorial.vue
@@ -1,12 +1,22 @@
 <template>
   <figure class="image">
+    <iframe
+      v-if="isYoutube"
+      width="100%"
+      height="415"
+      :src="`https://www.youtube-nocookie.com/embed/${videoId}`"
+      title="YouTube video player"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowfullscreen
+    />
     <video
+      v-else
       ref="video"
       controls
       width="100%"
     >
       <source
-        :src="exercise.source"
+        :src="url"
         type="video/mp4"
       >
     </video>
@@ -23,11 +33,32 @@ export default {
     },
   },
 
+  computed: {
+    url () {
+      return this.exercise.attributes?.video_url
+    },
+    videoId () {
+      return this.getId(this.exercise.attributes?.video_url)
+    },
+    isYoutube () {
+      return !!this.videoId
+    },
+  },
+
   mounted () {
     const self = this
-    this.$refs.video.addEventListener('ended', () => {
-      self.$emit('finished')
-    })
+    self.$emit('finished')
+  },
+
+  methods: {
+    getId (url = '') {
+      const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]*).*/
+      const match = url.match(regExp)
+
+      return (match && match[2].length === 11)
+        ? match[2]
+        : null
+    },
   },
 }
 </script>
